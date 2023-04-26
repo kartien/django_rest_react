6668b4187d0cd9bb50a8cdcb93b962046c8d0e1a
kartien <kevinramon523@gmail.com>
2023-04-25 22:31:03 -0500

    database add

--- Readme.md
+++ Readme.md
@@ -1,10 +1,24 @@
 # Dajngo Rest with React 
 
-## Start project Django
-```bahs 
-pip install -r requirements.txt
-```
-## Run optional script
+## Recommend run script on install and run project 
 ```bash
 chmod +x start.sh
+./start.sh
 ```
+
+## Start project Django
+```bash
+git clone 
+```
+## Install requirements.txt
+```bash
+pip -r install requirements.txt
+```
+## Run project
+```bash
+python manage.py makemigrations
+python manage.py migrate 
+python manage.py runserver
+```
+
+
--- start.sh
+++ start.sh
@@ -1,2 +1,22 @@
-#!bin/bash
+#1/bin/bash
+
+echo "  _   _            _       "          
+echo " ( ) ( )          ( )_ _    "         
+echo " | |/ /   _ _ _ __|  _)_)  __   ___  "
+echo " |   (  / _  )  __) | | |/ __ \  _  \ "
+echo " | |\ \( (_| | |  | |_| |  ___/ ( ) | "
+echo " (_) (_)\__ _)_)   \__)_)\____)_) (_) "
+
+echo "Install requirements.txt"
+pip install -r requirements.txt
+
+echo "Makemigrations"
+python manage.py makemigrations
+
+echo "Migrate"
+python manage.py migrate
+
+echo "Runserver" 
+python manage.py runserver 3000
+
 
